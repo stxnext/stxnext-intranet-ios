430070ef87b905de1b06787f9ae28853ce9f6e4d
Dawid Żakowski <dawid.zakowski@stxnext.pl>
2013-11-13 13:11:59 +0100

    Storyboard dla iPada.

---
+++ Intranet/UserDetailsSegue.h
@@ -0,0 +1,13 @@
+//
+//  UserDetailsSegue.h
+//  Intranet
+//
+//  Created by Dawid Żakowski on 13/11/2013.
+//  Copyright (c) 2013 STXNext. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserDetailsSegue : UIStoryboardSegue
+
+@end
