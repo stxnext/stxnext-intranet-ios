dda186cd1e56e4f5e3ec58e7ea42128a8f3c5619
Dawid Żakowski <dawid.zakowski@stxnext.pl>
2013-11-13 13:31:19 +0100

    Poprawka dla widoku dzielonego na iPadzie w orientacji pionowej.

---
+++ Intranet/AppDelegate+SplitControllerDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+SplitControllerDelegate.h
+//  Intranet
+//
+//  Created by Dawid Żakowski on 13/11/2013.
+//  Copyright (c) 2013 STXNext. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (SplitControllerDelegate) <UISplitViewControllerDelegate>
+
+@end
