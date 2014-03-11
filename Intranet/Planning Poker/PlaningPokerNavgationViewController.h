cf1d0329313bd92ff4c89becc90d4816d5fa15c2
Adam Pieskiewicz <adam.pieskiewicz@stxnext.pl>
2014-03-11 08:49:08 +0100

    Początek storyborard’a dla PlanningPokera

---
+++ Intranet/Planning Poker/PlaningPokerNavgationViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaningPokerNavgationViewController.h
+//  Intranet
+//
+//  Created by Adam on 11.03.2014.
+//  Copyright (c) 2014 STXNext. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaningPokerNavgationViewController : UINavigationController
+
+@end
---
+++ Intranet/Planning Poker/PlaningPokerNavgationViewController.m
@@ -0,0 +1,65 @@
+//
+//  PlaningPokerNavgationViewController.m
+//  Intranet
+//
+//  Created by Adam on 11.03.2014.
+//  Copyright (c) 2014 STXNext. All rights reserved.
+//
+
+#import "PlaningPokerNavgationViewController.h"
+
+@interface PlaningPokerNavgationViewController ()
+
+@end
+
+@implementation PlaningPokerNavgationViewController
+
+
+- (id)initWithRootViewController:(UIViewController *)rootViewController
+{
+    self = [super initWithRootViewController:rootViewController];
+    
+    if (self) {
+        
+    }
+    
+    return self;
+    
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.viewControllers = @[[[UIStoryboard storyboardWithName:@"PlanningPokerStoryboard" bundle:nil] instantiateInitialViewController]];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
