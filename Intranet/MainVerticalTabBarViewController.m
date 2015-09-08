29d41278a451c48c8b457e54615384de7ee9a007
Tomasz Walenciak <tomek.walenciak@gmail.com>
2015-09-08 15:15:26 +0200

    iPad - absences or out of office

--- Intranet/MainVerticalTabBarViewController.m
+++ Intranet/MainVerticalTabBarViewController.m
@@ -9,15 +9,14 @@
 #import "MainVerticalTabBarViewController.h"
 
 #import "TabIconTableViewCell.h"
+#import "AddOOOFormTableViewController.h"
 
 #import "UIImage+Color.h"
 
-@interface MainVerticalTabBarViewController ()
+@interface MainVerticalTabBarViewController ()<UIActionSheetDelegate>
 
 @property (nonatomic) NSArray *modelImagesData;
-
 @property (nonatomic) UITabBarController *embededTabBarController;
-
 @property (nonatomic) NSUInteger selectedRow;
 
 @end
@@ -49,7 +48,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark
+#pragma mark UITableView Delegate/Data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
@@ -80,10 +79,35 @@
     cell.selected = indexPath.row == _selectedRow;
 }
 
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 5) {
+        
+        UIActionSheet *popoverDelay = [[UIActionSheet alloc]initWithTitle:NSLocalizedString(@"Absence", nil)
+                                                                 delegate:self
+                                                        cancelButtonTitle:NSLocalizedString(@"Cancel", nil)
+                                                   destructiveButtonTitle:nil
+                                                        otherButtonTitles:@"Absence / Holiday", @"Out of office", nil];
+        CGRect rectCell = [tableView rectForRowAtIndexPath:indexPath];
+        
+        CGRect rect = [tableView convertRect:rectCell toView:self.view];
+        
+        [popoverDelay showFromRect:rect inView:self.view animated:YES];
+        return [NSIndexPath indexPathForRow:_selectedRow inSection:0];
+    }
+    return indexPath;
+}
+
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    _selectedRow = indexPath.row;
-    self.embededTabBarController.selectedIndex = indexPath.row;
+    
+    if (indexPath.row == 5) {
+
+        
+    } else {
+        _selectedRow = indexPath.row;
+        self.embededTabBarController.selectedIndex = indexPath.row;
+    }
 }
 
 #pragma mark - Navigation
@@ -92,6 +116,32 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([segue.identifier isEqualToString:@"content_embed_segue"]) {
         self.embededTabBarController = segue.destinationViewController;
+    }
+}
+
+#pragma mark - UIActionSheetDelegate
+
+
+- (void)actionSheetCancel:(UIActionSheet *)actionSheet
+{
+    
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    
+    if (buttonIndex < 2) {
+        _selectedRow = 5;
+        self.embededTabBarController.selectedIndex = _selectedRow;
+        NSIndexPath *absencesIP = [NSIndexPath indexPathForRow:_selectedRow inSection:0];
+        [self.verticalBarTableView selectRowAtIndexPath:absencesIP animated:YES scrollPosition:UITableViewScrollPositionNone];
+    }
+    
+    if (buttonIndex == 1) {
+        UINavigationController *navVc = self.embededTabBarController.viewControllers[5];
+        
+        AddOOOFormTableViewController *form = (AddOOOFormTableViewController *)[navVc.viewControllers firstObject];
+        form.currentRequest = (int)buttonIndex;
     }
 }
 
